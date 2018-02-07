c835497aa1089d41c0c5b49dc03dd56a30e5efb6
quinlayen <quinlayen@gmail.com>
2018-02-07 00:31:50 -1000

    base planets and starships complete.  Need to complete nested data on both sets

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@ function request(method, url, handler) {
 const submitButton = document.getElementById("requestResourceButton");
 
 submitButton.addEventListener("click", function() {
-  const h2 = document.createElement('h2');
-  const container = document.getElementById('contentContainer');
+  const h2 = document.createElement("h2");
+  const container = document.getElementById("contentContainer");
   const resourceType = document.getElementById("resourceType");
   let category = resourceType.options[resourceType.selectedIndex].text;
   if (category === "Person") {
@@ -24,38 +24,53 @@ submitButton.addEventListener("click", function() {
   const categoryId = document.getElementById("resourceId").value;
   const url = `https://swapi.co/api/${category}/${categoryId}`;
   const speciesUrl = `https://swapi.co/api/species/1`;
-  
+  const filmsUrl = `https://swapi.co/api/films`
   //switch statement to determine which category has been selected and then creates request based on the category
   switch (category) {
     case "people":
       request("GET", url, function() {
         const peopleData = JSON.parse(this.responseText);
-        const gender = document.createElement('p');
+        const gender = document.createElement("p");
         container.appendChild(h2).innerHTML = peopleData.name;
         container.appendChild(gender).innerHTML = peopleData.gender;
-        request("GET", speciesUrl, function(){
-            const speciesData = JSON.parse(this.responseText)
-            const species = document.createElement('p');
-             container.appendChild(species).innerHTML = speciesData.name;
-         })
-        
+        request("GET", speciesUrl, function() {
+          const speciesData = JSON.parse(this.responseText);
+          const species = document.createElement("p");
+          container.appendChild(species).innerHTML = speciesData.name;
+        });
       });
       break;
     case "planets":
       request("GET", url, function() {
-        const data = JSON.parse(this.responseText);
-        console.log(data);
-        container.appendChild(h2).innerHTML = data.name;
-
-
+        const planetsData = JSON.parse(this.responseText);
+        const terrain = document.createElement("p");
+        const population = document.createElement("p");
+        container.appendChild(h2).innerHTML = planetsData.name;
+        container.appendChild(terrain).innerHTML = planetsData.terrain;
+        container.appendChild(population).innerHTML = planetsData.population;
+        request("GET", filmsUrl, function(){
+            const filmsData = JSON.parse(this.responseText);
+            const filmsDataResults = filmsData.results;
+            const filmsList = document.createElement("ul");
+            const mappedList = filmsDataResults.map(function(element){
+                let film = document.createElement("li");
+                container.appendChild(filmsList);
+                filmsList.appendChild(film).innerHTML = element.title
+                
+            });
+            
+        });
       });
       break;
     case "starships":
       request("GET", url, function() {
-        const data = JSON.parse(this.responseText);
-        console.log(data);
-        container.appendChild(h2).innerHTML = data.name;
-
+        const starshipsData = JSON.parse(this.responseText);
+        console.log(starshipsData)
+        const manufacturer = document.createElement("p");
+        const starshipClass = document.createElement("p");
+        container.appendChild(h2).innerHTML = starshipsData.name;
+        container.appendChild(manufacturer).innerHTML = starshipsData.manufacturer;
+        container.appendChild(starshipClass).innerHTML = starshipsData.starship_class;
 
       });
       break;
