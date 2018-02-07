4736409214cfe5987480d121448da50449c77502
quinlayen <quinlayen@gmail.com>
2018-02-06 22:34:10 -1000

    people case functional.  now work on planets

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ function request(method, url, handler) {
 const submitButton = document.getElementById("requestResourceButton");
 
 submitButton.addEventListener("click", function() {
+  const h2 = document.createElement('h2');
+  const container = document.getElementById('contentContainer');
   const resourceType = document.getElementById("resourceType");
   let category = resourceType.options[resourceType.selectedIndex].text;
   if (category === "Person") {
@@ -21,25 +23,40 @@ submitButton.addEventListener("click", function() {
   }
   const categoryId = document.getElementById("resourceId").value;
   const url = `https://swapi.co/api/${category}/${categoryId}`;
-
+  const speciesUrl = `https://swapi.co/api/species/1`;
+  
   //switch statement to determine which category has been selected and then creates request based on the category
   switch (category) {
     case "people":
       request("GET", url, function() {
-        const data = JSON.parse(this.responseText);
-        console.log(data);
+        const peopleData = JSON.parse(this.responseText);
+        const gender = document.createElement('p');
+        container.appendChild(h2).innerHTML = peopleData.name;
+        container.appendChild(gender).innerHTML = peopleData.gender;
+        request("GET", speciesUrl, function(){
+            const speciesData = JSON.parse(this.responseText)
+            const species = document.createElement('p');
+             container.appendChild(species).innerHTML = speciesData.name;
+         })
+        
       });
       break;
     case "planets":
       request("GET", url, function() {
         const data = JSON.parse(this.responseText);
         console.log(data);
+        container.appendChild(h2).innerHTML = data.name;
+
+
       });
       break;
     case "starships":
       request("GET", url, function() {
         const data = JSON.parse(this.responseText);
         console.log(data);
+        container.appendChild(h2).innerHTML = data.name;
+
+
       });
       break;
   }
