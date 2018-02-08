7b888375fe2658629fa6ba1a220abc1392f71701
quinlayen <quinlayen@gmail.com>
2018-02-07 21:05:27 -1000

    updated callback function to include the json.parse so that it is not necessary to write every time we call request

--- js/app.js
+++ js/app.js
@@ -1,65 +1,65 @@
-console.log("sanity check");
+console.log('sanity check');
 //helper function to create requests dynamically
 function request(method, url, callback) {
   const oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", callback);
+  oReq.addEventListener('load', function() {
+    const data = JSON.parse(this.responseText);
+    return callback.bind(this)(data);
+  });
   oReq.open(method, url);
   oReq.send();
 }
 //clear the Dom
-function clearDom(){
+function clearDom() {
   document.removeChild(h2);
-  document.removeChild(p)
+  document.removeChild(p);
 }
 
-const submitButton = document.getElementById("requestResourceButton");
+const submitButton = document.getElementById('requestResourceButton');
 
-submitButton.addEventListener("click", function() {
-  
-  const h2 = document.createElement("h2");
-  const container = document.getElementById("contentContainer");
-  const resourceType = document.getElementById("resourceType");
+submitButton.addEventListener('click', function() {
+  const h2 = document.createElement('h2');
+  const container = document.getElementById('contentContainer');
+  const resourceType = document.getElementById('resourceType');
   let category = resourceType.options[resourceType.selectedIndex].text;
-  if (category === "Person") {
-    category = "people";
-  } else if (category === "Planet") {
-    category = "planets";
-  } else {1
-    category = "starships";
+  if (category === 'Person') {
+    category = 'people';
+  } else if (category === 'Planet') {
+    category = 'planets';
+  } else {
+    1;
+    category = 'starships';
   }
-  const categoryId = document.getElementById("resourceId").value;
+  const categoryId = document.getElementById('resourceId').value;
   const url = `https://swapi.co/api/${category}/${categoryId}/`;
   const speciesUrl = `https://swapi.co/api/species/1`;
   const filmsUrl = `https://swapi.co/api/films`;
   //switch statement to determine which category has been selected and then creates request based on the category
   switch (category) {
-    case "people":
-      request("GET", url, function() {
-        console.log(url)
-        const peopleData = JSON.parse(this.responseText);
-        const gender = document.createElement("p");
-        container.appendChild(h2).innerHTML = peopleData.name;
-        container.appendChild(gender).innerHTML = peopleData.gender.charAt(0).toUpperCase() + peopleData.gender.slice(1);
-        request("GET", speciesUrl, function() {
-          const speciesData = JSON.parse(this.responseText);
-          const species = document.createElement("p");
-          container.appendChild(species).innerHTML = speciesData.name;
+    case 'people':
+      request('GET', url, function(data) {
+        const gender = document.createElement('p');
+        container.appendChild(h2).innerHTML = data.name;
+        container.appendChild(gender).innerHTML =
+          data.gender.charAt(0).toUpperCase() + data.gender.slice(1);
+        request('GET', speciesUrl, function() {
+          const species = document.createElement('p');
+          container.appendChild(species).innerHTML = data.name;
         });
       });
       break;
-    case "planets": 
-      request("GET", url, function() {
-        const planetsData = JSON.parse(this.responseText);
-        const terrain = document.createElement("p");
-        const population = document.createElement("p");
-        container.appendChild(h2).innerHTML = planetsData.name;
-        container.appendChild(terrain).innerHTML = planetsData.terrain.charAt(0).toUpperCase() + planetsData.terrain.slice(1);
-        container.appendChild(population).innerHTML = planetsData.population;
-        const planetFilmList = planetsData.films.map(function(element) {
-          request("GET", element, function() {
-            const planetFilmsData = JSON.parse(this.responseText);
-            const filmsList = document.createElement("ul");
-            let film = document.createElement("li");
+    case 'planets':
+      request('GET', url, function(data) {
+        const terrain = document.createElement('p');
+        const population = document.createElement('p');
+        container.appendChild(h2).innerHTML = data.name;
+        container.appendChild(terrain).innerHTML =
+          data.terrain.charAt(0).toUpperCase() + data.terrain.slice(1);
+        container.appendChild(population).innerHTML = data.population;
+        const planetFilmList = data.films.map(function(element) {
+          request('GET', element, function(data) {
+            const filmsList = document.createElement('ul');
+            let film = document.createElement('li');
             container.appendChild(filmsList);
             filmsList.appendChild(film).innerHTML = planetFilmsData.title;
           });
@@ -67,23 +67,21 @@ submitButton.addEventListener("click", function() {
       });
 
       break;
-    case "starships":
-      request("GET", url, function() {
-        const starshipsData = JSON.parse(this.responseText);
-        //console.log(starshipsData.films[1]);
-        const manufacturer = document.createElement("p");
-        const starshipClass = document.createElement("p");
-        container.appendChild(h2).innerHTML = starshipsData.name;
-        container.appendChild(manufacturer).innerHTML = starshipsData.manufacturer;
-        container.appendChild(starshipClass).innerHTML = starshipsData.starship_class.charAt(0).toUpperCase() + starshipsData.starship_class.slice(1);;
-        const starshipsFilmList = starshipsData.films.map(function(element){
-          request("GET", element, function(){
-            const starshipsFilmsData = JSON.parse(this.responseText);
-            console.log(starshipsFilmsData)
-            const filmsList = document.createElement("ul");
-            let film = document.createElement("li");
+    case 'starships':
+      request('GET', url, function(data) {
+        const manufacturer = document.createElement('p');
+        const starshipClass = document.createElement('p');
+        container.appendChild(h2).innerHTML = data.name;
+        container.appendChild(manufacturer).innerHTML = data.manufacturer;
+        container.appendChild(starshipClass).innerHTML =
+          data.starship_class.charAt(0).toUpperCase() +
+          data.starship_class.slice(1);
+        const starshipsFilmList = data.films.map(function(element) {
+          request('GET', element, function(data) {
+            const filmsList = document.createElement('ul');
+            let film = document.createElement('li');
             container.appendChild(filmsList);
-            filmsList.appendChild(film).innerHTML = starshipsFilmsData.title;
+            filmsList.appendChild(film).innerHTML = data.title;
           });
         });
       });
